914e47a556facd58a1597602c86652ff50dfd863
nick <nikyasnogorodskyi@gmail.com>
2021-09-12 21:16:43 +0300

    chore: hustky

--- src/resolvers/UserPositionResolver/index.ts
+++ src/resolvers/UserPositionResolver/index.ts
@@ -1,26 +1,23 @@
-import { Arg, Ctx, Field, Float, InputType, Mutation, PubSub, PubSubEngine, Resolver, UseMiddleware } from 'type-graphql';
-import { isAuthenticated } from '../../middlewares/isAuthenticated';
-import { CustomContext } from '../../types/customContext';
-import { SuccessResponse } from '../../types/SuccessResponse';
-import { PositionArgs, UserLocation } from '../../utils/redis/location';
+import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from 'type-graphql'
+import { isAuthenticated } from '../../middlewares/isAuthenticated'
+import { CustomContext } from '../../types/customContext'
+import { SuccessResponse } from '../../types/SuccessResponse'
+import { PositionArgs, UserLocation } from '../../utils/redis/location'
 
 @Resolver(SuccessResponse)
 export class UserPositionResolver {
-
     @UseMiddleware(isAuthenticated)
     @Mutation(() => SuccessResponse)
     async updatePosition(
-        @Arg("input") position: PositionArgs,
-        @Ctx() ctx: CustomContext,
-        @PubSub() pubSub: PubSubEngine
+        @Arg('input') position: PositionArgs,
+        @Ctx() ctx: CustomContext
     ): Promise<SuccessResponse> {
         const { user } = ctx
 
         const location = new UserLocation(user.id)
 
         location.set(position)
-        // await pubSub.publish(LOCATION_UPDATE, payload);
 
-        return { success: true };
+        return { success: true }
     }
 }
